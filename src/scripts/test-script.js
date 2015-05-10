76b5b3903591db59ab34c84e93cbf26d849a5947
Caleb Morris <caleb.morris.g@gmail.com>
2015-05-09 19:02:17 -0700

    Added a temporary ban button

--- src/scripts/test-script.js
+++ src/scripts/test-script.js
@@ -3,17 +3,35 @@ console.log('test script loaded');
 var timer = 1;
 var selectors;
 
+function onBanClick(url, event) {
+  debugger;
+}
+
+function generateBanButton(url) {
+  var banner = document.createElement('label');
+  banner.appendChild(document.createTextNode('B'));
+  banner.setAttribute('class', 'banbutton');
+
+  banner.addEventListener('click', onBanClick.bind(this, url));
+
+  return banner;
+}
+
 function onLoaded() {
   console.log('selectors', selectors);
 
   for(var i = 0; i < selectors.length; i++) {
     var item = selectors[i];
-    var linkInfo = item.querySelector('.r a');
-    if (linkInfo) {
-      console.log('item', linkInfo.href);
-      if (linkInfo.href === 'http://www.speedtest.net/') {
-        item.setAttribute('hidden', 'hidden'); // Example on how to hide an item
-      }
+    var linkWrapper = item.querySelector('.r');
+    if (! linkWrapper) { continue; }
+    var linkInfo = linkWrapper.firstChild;
+    if (! linkInfo) { continue; }
+
+    console.log('item', linkInfo.href);
+    if (linkInfo.href === 'http://www.speedtest.net/') {
+      item.setAttribute('hidden', 'hidden'); // Example on how to hide an item
+    } else {
+      linkWrapper.insertBefore(generateBanButton(linkInfo.href), linkInfo);
     }
   }
 }
