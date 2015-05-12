0a7c41f1f281306fc6a6777d90c58f0d74ee0ae5
Caleb Morris <caleb.morris.g@gmail.com>
2015-05-11 23:00:23 -0700

    Added table dispay of currently banned sites

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,4 +1,7 @@
-console.log('popup script');
+var clearButton = document.getElementById('clear-button');
+var banlistWrapper = document.getElementById('banlist-wrapper');
+var loadingMessage = document.getElementById('loading-message');
+var storage = null;
 
 function onClearBanlist(cb) {
   chrome.storage.sync.clear(function() {
@@ -15,10 +18,35 @@ function loadStorage(cb) {
   });
 }
 
-loadStorage(function(err, items) {
-  console.log('items', JSON.stringify(items));
-});
+function generateBanlistRow(url) {
+  var row = document.createElement('tr');
+  var element = document.createElement('td');
+  var text = document.createTextNode(url);
+  element.appendChild(text);
+  row.appendChild(element);
 
-var clearButton = document.getElementById('clear-button');
+  return row;
+}
+
+function generateBanlistTable(bans) {
+  var table = document.createElement('table');
+  for(var i = 0; i < bans.length; i++) {
+    table.appendChild(generateBanlistRow(bans[i]));
+  }
+  banlistWrapper.appendChild(table);
+}
+
+loadStorage(function(err, items) {
+  if (err) {
+    throw err;
+  }
+  storage = items;
+  banlistWrapper.removeChild(loadingMessage);
+  if (storage.bans && storage.bans.length > 0) {
+    generateBanlistTable(storage.bans);
+  } else {
+    banlistWrapper.appendChild(document.createTextNode('You have no bans'));
+  }
+});
 
 clearButton.addEventListener('click', onClearBanlist);
